e20ca62ad3261eb6f0c513c1e3a84c0f9d7f5c0b
djtoohey <djtoohey@gmail.com>
2020-06-17 14:17:34 +0930

    added comments to script.js

--- Assets/script.js
+++ Assets/script.js
@@ -1,21 +1,30 @@
+// gets the current hour
 hour = parseInt(moment().format("HH"));
 
+// loops through times
 for (let i = 9; i < (18); i++) {
+    // gets div with time i
     var div = $("div").find(`[data-time='${i}']`);
+
+    // doesnt work
     var btn = $("button").find(`[data-time=${i}]`);
     console.log(btn);
     btn.click(function () {
         console.log(i);
     });
+    // end doesnt work
 
+    // sets time in the past
     if (i < hour) {
         div.addClass("past");
         // console.log("past");
     }
+    // sets time in the future
     else if (i > hour) {
         div.addClass("future");
         // console.log("future");
     }
+    // sets time in the present
     else {
         div.addClass("present");
         // console.log("present");
